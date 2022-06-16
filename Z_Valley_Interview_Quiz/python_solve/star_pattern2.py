3b3004eb18847bce56e4f1dba07cdbb1beb8b458
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-17 00:01:56 +0700

    Create star_pattern2.py

---
+++ Z_Valley_Interview_Quiz/python_solve/star_pattern2.py
@@ -0,0 +1,9 @@
+star = 5
+k = 2 * star - 2
+for i in range(0, star):
+    for j in range(0, k):
+        print(end=" ")
+    k = k - 2
+    for j in range(0, i + 1):
+        print("* ", end="")
+    print("\r")
